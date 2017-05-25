e5b98070859484419696af09a29f73ac26e404ef
Ade Oladejo <adeoladejo@gmail.com>
2017-05-25 16:54:02 +0100

    included new queries

--- tutorial.sql
+++ tutorial.sql
@@ -34,14 +34,53 @@ values(2,"Manny Paki" , 21 , "51 Brayards road" );
 
 desc employer;
 
-insert into employer values(1 , "Reacy Beaker", "Software engineer" , 15000.00);
-insert into employer values(2 , "cunt Beaker", "Human Resource" , 23000.00);
-insert into employer values(3 , "Reacy", "Software engineer" , 13000.00);
+  
+insert into employer values(1 , "Webastian Edge Wolos", "Deowit internship" , 20000.00);
+insert into employer values(2 , "Warren Windall", "Iwan advisor" , 0.00);
+insert into employer values(3 , "Ade Oladejo", "Unemployed" , 0.00);
 insert into employer values(4 , "cunt", "Human Resource" , 33000.00);
 insert into employer values(5 , "Ade", "Software engineer" , 34000.00);
 
 select * from employer
 where salary > 20000;
+
+select* from employer
+where role = "Software Engineer";
+
+select * from employer 
+where 
+salary <= 20000 OR salary >= 10000;
+
+update employer 
+set role = "Financial Director" , salary = 52000.00
+where id = 2;
+
+select * from employer;
+
+update employer 
+set role = "Entry level Salary"
+where id = 6;
+
+select * from employer;
+
+create table customers
+(
+	ID int not null,
+    first_name varchar(60) ,
+    last_name varchar(60) ,
+    age int,
+    primary key(ID) 
+);
+
+create table ORDERS
+(
+	ID int not null , 
+    order_numer int not null,
+    customer_ID int,
+    primary key(ID) ,
+    FOREIGN KEY(customer_ID) REFERENCES CUSTOMERS(ID)
+);
+
 
 
 
